e8dd2fc0f312fafa0b802f46b875d715b338bf2c
Jonathon Hill <jhill9693@gmail.com>
2019-08-13 23:07:45 -0400

    Use peerDependencies to allow ES version flexibility
    Resolves #18 and upgrades dependencies for security

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
-'use strict';
-
+const assert = require('assert');
 const AWS = require('aws-sdk');
 const elasticsearch = require('elasticsearch');
 const httpAWSES = require('http-aws-es');
 
-function createClient(options) {
-	options = options || {}; // eslint-disable-line no-param-reassign
+function createClient(options = {}) {
+	assert(typeof options === 'object', 'options must be an object');
 
 	const region = options.region || process.env.AWS_REGION || 'us-east-1';
-	const config = Object.assign(
-		{},
-		options,
-		{
-			connectionClass: httpAWSES,
-			awsConfig: new AWS.Config({
-				region,
-				credentials: options.credentials
-			})
-		}
-	);
+
+	const config = {
+		...options,
+		connectionClass: httpAWSES,
+		awsConfig: new AWS.Config({
+			region,
+			credentials: options.credentials
+		})
+	};
 
 	return new elasticsearch.Client(config);
 }
